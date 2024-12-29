cb241b7caad160dae8f4f3ce1fbbd60ebe25aa63
Gukin Han <gukin.dev@gmail.com>
2024-12-29 11:59:20 +0900

    도커파일 스프링애플리케이션 예제

---
+++ java/spring/docker-example-1/src/test/java/com/example/dockerexample1/DockerExample1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockerexample1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerExample1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
