818a30c8c1253ed9287ddb1ceb2d6ffc6238b761
Gukin Han <gukin.dev@gmail.com>
2025-05-22 22:39:50 +0900

    init: java spring core basic

---
+++ java-spring-core-basic/src/main/java/com/example/javaspringcorebasic/JavaSpringCoreBasicApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaspringcorebasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringCoreBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringCoreBasicApplication.class, args);
+	}
+
+}
