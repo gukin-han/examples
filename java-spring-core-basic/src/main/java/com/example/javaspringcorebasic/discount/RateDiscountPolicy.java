7e8978731e4fb1f015af8a58bd7f9ba48bfe33aa
Gukin Han <gukin.dev@gmail.com>
2025-05-25 22:31:25 +0900

    17. 새로운 할인 정책 개발

---
+++ java-spring-core-basic/src/main/java/com/example/javaspringcorebasic/discount/RateDiscountPolicy.java
@@ -0,0 +1,18 @@
+package com.example.javaspringcorebasic.discount;
+
+import com.example.javaspringcorebasic.member.Grade;
+import com.example.javaspringcorebasic.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy{
+
+    private int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+}
