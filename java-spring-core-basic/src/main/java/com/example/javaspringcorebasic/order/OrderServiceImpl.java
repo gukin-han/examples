29da014af997ccbdf946bbfb6dc22d389e652aae
Gukin Han <gukin.dev@gmail.com>
2025-05-25 22:54:11 +0900

    18. 새로운 할인 정책 적용과 문제점

--- java-spring-core-basic/src/main/java/com/example/javaspringcorebasic/order/OrderServiceImpl.java
+++ java-spring-core-basic/src/main/java/com/example/javaspringcorebasic/order/OrderServiceImpl.java
@@ -1,7 +1,6 @@
 package com.example.javaspringcorebasic.order;
 
 import com.example.javaspringcorebasic.discount.DiscountPolicy;
-import com.example.javaspringcorebasic.discount.FixDiscountPolicy;
 import com.example.javaspringcorebasic.member.Member;
 import com.example.javaspringcorebasic.member.MemberRepository;
 import com.example.javaspringcorebasic.member.MemoryMemberRepository;
@@ -9,7 +8,10 @@ import com.example.javaspringcorebasic.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    // DIP 위반 : 인터페이스와 구체 클래스를 모두 의존성을 갖고 있다 -> 인터페이스만 의존해야한다
+    // OCP 위반 : Fix -> Rate 변경은 클라이언트 소스를 함께 변경해야함
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private DiscountPolicy discountPolicy;
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
