b2b634a107ff8fd895fe47954e9755fdc831f249
Gukin Han <gukin.dev@gmail.com>
2025-06-06 00:12:05 +0900

    54. 인터페이스 InitializingBean, DisposableBean

--- java-spring-core-basic/src/test/java/com/example/javaspringcorebasic/lifecycle/NetworkClient.java
+++ java-spring-core-basic/src/test/java/com/example/javaspringcorebasic/lifecycle/NetworkClient.java
@@ -1,13 +1,14 @@
 package com.example.javaspringcorebasic.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
 
   private String url;
 
   public NetworkClient() {
     System.out.println("생성자 호출, url = " + url); // 아직 값이 없음
-    connect(); // 아직 값이 없음
-    call("초기화 연결 메시지");
   }
 
   public void setUrl(String url) {
@@ -25,5 +26,17 @@ public class NetworkClient {
 
   public void disconnect() {
     System.out.println("close: " + url);
+  }
+
+  @Override
+  public void afterPropertiesSet() throws Exception {
+    System.out.println("NetworkClient.afterPropertiesSet");
+    connect(); // 아직 값이 없음
+    call("초기화 연결 메시지");
+  }
+
+  @Override
+  public void destroy() throws Exception {
+    disconnect();
   }
 }
