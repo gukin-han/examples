796d2fa3a663f9e6de65a78464c2e7eb62f25173
Gukin Han <gukin.dev@gmail.com>
2025-06-15 23:20:55 +0900

    oauth init

---
+++ oauth/oauth-back/src/test/java/com/example/oauth_back/OauthBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oauth_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
