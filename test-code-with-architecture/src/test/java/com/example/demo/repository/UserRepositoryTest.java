54aa8deca011a54e183b0c3a850469f369c3d2a2
Gukin Han <gukin.dev@gmail.com>
2025-07-01 22:28:56 +0900

    10. 테스트 추가하기: h2를 이용한 repository 테스트

--- test-code-with-architecture/src/test/java/com/example/demo/repository/UserRepositoryTest.java
+++ test-code-with-architecture/src/test/java/com/example/demo/repository/UserRepositoryTest.java
@@ -1,10 +1,66 @@
 package com.example.demo.repository;
 
+import com.example.demo.model.UserStatus;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Optional;
 
 @DataJpaTest
+@Sql("/sql/user-repository-test-data.sql")
 class UserRepositoryTest {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @DisplayName("findByIdAndStatus로 유저데이터를 찾아올 수 있다")
+    @Test
+    void test2(){
+        //given
+        //when
+        Optional<UserEntity> result = userRepository.findByIdAndStatus(1, UserStatus.ACTIVE);
+
+        //then
+        Assertions.assertThat(result.isPresent()).isTrue();
+    }
+
+    @DisplayName("findByIdAndStatus는 데이터가 없으면 Optional empty를 내려준다")
+    @Test
+    void test3(){
+        //given
+
+        //when
+        Optional<UserEntity> result = userRepository.findByIdAndStatus(1, UserStatus.PENDING);
+
+        //then
+        Assertions.assertThat(result.isEmpty()).isTrue();
+    }
+
+    @DisplayName("findByEmailAndStatus로 유저데이터를 찾아올 수 있다")
+    @Test
+    void test4(){
+        //given
+
+        //when
+        Optional<UserEntity> result = userRepository.findByEmailAndStatus("test@naver.com", UserStatus.ACTIVE);
+
+        //then
+        Assertions.assertThat(result.isPresent()).isTrue();
+    }
+
+    @DisplayName("findByEmailAndStatus는 데이터가 없으면 Optional empty를 내려준다")
+    @Test
+    void test5(){
+        //given
+
+        //when
+        Optional<UserEntity> result = userRepository.findByEmailAndStatus("test@naver.com", UserStatus.PENDING);
+
+        //then
+        Assertions.assertThat(result.isEmpty()).isTrue();
+    }
 }
---
+++ test-code-with-architecture/src/test/resources/sql/user-repository-test-data.sql
@@ -0,0 +1,5 @@
+insert into users
+(id, email, nickname, address, certification_code, status, last_login_at)
+values
+    (1, 'test@naver.com', 'gukin', 'Seoul',
+     'aaaaa-aaaaa-aaaaaa-aaaaa-aaaaa-aaaa', 'ACTIVE', 0);
