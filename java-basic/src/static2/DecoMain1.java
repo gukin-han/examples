9b028be2843c238463f569a7203937d4aabf8884
Gukin Han <gukin.dev@gmail.com>
2025-07-07 22:23:29 +0900

    56. static 메서드1

---
+++ java-basic/src/static2/DecoMain1.java
@@ -0,0 +1,10 @@
+package static2;
+
+public class DecoMain1 {
+
+    public static void main(String[] args) {
+        String s = "hello java";
+        DecoUnit1 utils = new DecoUnit1();
+        String deco = utils.deco(s);
+    }
+}
